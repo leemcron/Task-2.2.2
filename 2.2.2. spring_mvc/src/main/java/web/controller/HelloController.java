02d89d0eb1e8d4232877b9cb46db8770f3d56130
leemcron <mr.palevo95@gmail.com>
2020-10-13 08:52:31 +0300

    Commit 13.10

--- 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,7 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-	@GetMapping(value = "/")
+	@GetMapping( value = "/")
 	public String printWelcome(ModelMap model) {
 		List<String> messages = new ArrayList<>();
 		messages.add("Hello!");
