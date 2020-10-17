bac40c2c5ca0542f33ade9c4ac94e3c7eeb173f0
leemcron <mr.palevo95@gmail.com>
2020-10-17 13:18:15 +0300

    Commit 17.10

--- 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
+++ 2.2.2. spring_mvc/src/main/java/web/controller/CarsController.java
@@ -3,30 +3,21 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarsService;
+import web.service.CarsServiceImpl;
 
 @Controller
 public class CarsController {
 
+    final CarsService carsService = new CarsServiceImpl();
+
     @GetMapping(value = "/cars")
     public String printCars(@RequestParam(value = "count", required = false) Integer x, Model model) {
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("BMW", "green", 3));
-        cars.add(new Car("Reno", "red", 232));
-        cars.add(new Car("Audi", "black", 4));
-        cars.add(new Car("Mazda", "white", 6));
-        cars.add(new Car("Kia", "grey", 331));
-
-        if (x == null || x>5 ) {
-            model.addAttribute("cars", cars);
+        if (x == null) {
+            model.addAttribute("cars", carsService.listCars(5));
         } else {
-            List<Car> list = cars.subList(0, x);
-            model.addAttribute("cars",list);
+            model.addAttribute("cars", carsService.listCars(x));
         }
         return "cars";
     }
---
+++ 2.2.2. spring_mvc/src/main/java/web/service/CarsServiceImpl.java
@@ -0,0 +1,33 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarsServiceImpl implements CarsService {
+
+
+
+
+
+    @Override
+    public List<Car> listCars(int countCars) {
+        List<Car> cars = new ArrayList<>();
+        cars.add(new Car("BMW", "green", 3));
+        cars.add(new Car("Reno", "red", 232));
+        cars.add(new Car("Audi", "black", 4));
+        cars.add(new Car("Mazda", "white", 6));
+        cars.add(new Car("Kia", "grey", 331));
+
+        return countCars > 5 ? cars : cars.subList(0, countCars);
+    }
+
+
+    public static void main(String[] args) {
+        CarsService carsService = null;
+        assert carsService != null;
+        List<Car> cars = carsService.listCars(55);
+        cars.forEach(System.out::println);
+    }
+}
